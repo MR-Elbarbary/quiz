e00192bb14194e6e0666a70cda5946d165f30c62
MR-Elbarbary <121816435+MR-Elbarbary@users.noreply.github.com>
2023-12-20 22:31:20 +0200

    bug fixed

--- project/quiz.py
+++ project/quiz.py
@@ -1,13 +1,16 @@
 import sqlite3
 
+# Path to the database file
+db_path = 'project/quiz.db'  # Specify the relative path to the database file
+
 # Connect to the database
-conn = sqlite3.connect('quiz.db')
+conn = sqlite3.connect(db_path)
 
 # Create a cursor object to interact with the database
 cursor = conn.cursor()
 
 # Execute the SQL query
-cursor.execute("SELECT * FROM temp WHERE id = ?", (1,))
+cursor.execute("SELECT * FROM temp LIMIT 1")
 
 # Fetch the result
 result = cursor.fetchone()
