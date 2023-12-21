e97cb84b361f23d69e94215d89251613f80712f5
MR-Elbarbary <121816435+MR-Elbarbary@users.noreply.github.com>
2023-12-21 21:45:30 +0200

    the code now works version alpha

---
+++ project/index.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "quiz"(
+    "id" INTEGER,
+    "question" TEXT NOT NULL,
+    "a" TEXT NOT NULL,
+    "b" TEXT NOT NULL,
+    "c" TEXT NOT NULL,
+    "d" TEXT NOT NULL,
+    "answer" TEXT NOT NULL,
+    PRIMARY KEY("id")
+);
+
+INSERT INTO "quiz" ("question", "a", "b", "c", "d", "answer")
+SELECT "question", " choice a", " choice b", " choice c", " choice d", " answer" FROM temp;
--- project/quiz.py
+++ project/quiz.py
@@ -1,4 +1,5 @@
 import sqlite3
+import random
 
 # Path to the database file
 db_path = 'project/quiz.db'  # Specify the relative path to the database file
@@ -7,14 +8,43 @@ db_path = 'project/quiz.db'  # Specify the relative path to the database file
 conn = sqlite3.connect(db_path)
 
 # Create a cursor object to interact with the database
-cursor = conn.cursor()
+db = conn.cursor()
 
 # Execute the SQL query
-cursor.execute("SELECT * FROM temp LIMIT 1")
+db.execute("SELECT * FROM temp LIMIT 1")
 
 # Fetch the result
-result = cursor.fetchone()
+result = db.fetchone()
 print(result)
+
+n = -1
+while n not in range(1,71):
+    n = int(input("Enter how many questions from 1 to 70: "))
+array = []
+while len(array) < n:
+    temp = random.randint(1, 70)
+    if temp not in array:
+        array.append(temp)
+
+score = 0
+for number in array:
+    # Execute the SQL query
+    db.execute("SELECT * FROM quiz WHERE id = ?", (int(number),))
+
+    # Fetch the result
+    quiz = db.fetchone()
+    print(quiz[1])
+    print("a)",quiz[2])
+    print("b)",quiz[3])
+    print("c)",quiz[4])
+    print("d)",quiz[5])
+    ans = input()
+    if ans == quiz[6]:
+        score +=1
+        print("correct!")
+    else:
+        print("wrong")
+print(score)
 
 # Close the connection
 conn.close()
