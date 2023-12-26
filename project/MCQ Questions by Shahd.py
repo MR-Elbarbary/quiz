b6a9f90d299d96c330ba58a43ac6b2fdcb88cff9
Shahd-Akram <141468575+Shahd-Akram@users.noreply.github.com>
2023-12-26 15:52:23 +0200

    Create MCQ Questions by Shahd

---
+++ project/MCQ Questions by Shahd.py
@@ -0,0 +1,61 @@
+i =0
+ # Question 1
+print("Which of the following storage devices provides the fastest access to data in a computer system?\n") 
+print("1. Solid State Drive (SSD)\n2. Optical Disc Drive (ODD)\n3. USB Flash Drive\n4. External Hard Disk Drive (HDD) ")
+Q1 = int (input())
+    
+if Q1 == 1:
+ print("correct ! you got 1 point\n")
+ i +=1
+        
+else:
+ print("Incorrect")
+ print("The correct choice is 1.Solid State Drive (SSD) \n")
+ # Question 2
+print("Which of the following is responsible for managing and controlling the flow of data between the computer's hardware components?\n")
+print("1. Operating System\n2. Application Software\n3. BIOS (Basic Input/Output System)\n4. Motherboard")
+Q2 = int (input())
+    
+if Q2 == 1:
+ print("correct ! you got 1 point\n")
+ i +=1
+        
+else:
+ print("Incorrect")
+ print("The correct choice is 1. Operating System \n")
+# Question 3
+print(" Which of the following is an example of a secondary storage device in a computer system?\n")
+print("1. Random Access Memory (RAM)\n2. Central Processing Unit (CPU)\n3. Hard Disk Drive (HDD)\n4. Graphics Processing Unit (GPU)")
+Q3 = int (input())
+    
+if Q3 == 3:
+ print("correct ! you got 1 point\n")
+ i +=1
+        
+else:
+ print("Incorrect")
+ print("The correct choice is 3. Hard Disk Drive (HDD)\n")
+# Question 4
+print(" What does Cpu controls ?\n")
+print("1. All input,Output and processing\n2. Controlled by the input data\n3. Controls Memory\n4 .Output")
+Q4 = int (input())
+    
+if Q4 == 1:
+ print("correct ! you got 1 point\n")
+ i +=1
+        
+else:
+ print("Incorrect")
+ print("The correct choice is 1. All input,Output and processing \n")
+# Question 5
+print(" Waht is another name of ROM ?\n")
+print("1. Virtual memory\n2. Volatile memory\n3. Non volatile memory\n4. Cache memory")
+Q5 = int (input())
+    
+if Q5 == 3:
+ print("correct ! you got 1 point\n")
+ i +=1
+        
+else:
+ print("Incorrect")
+ print("The correct choice is 3. Non volatile memory \n")
